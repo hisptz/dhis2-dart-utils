07a52cb96e79bec6537d812c2bad0392b5b521b3
megamind <echingalo@gmail.com>
2022-09-27 12:06:36 +0300

    chore: project set up

---
+++ example/dhis2_dart_utils_example.dart
@@ -0,0 +1,6 @@
+import 'package:dhis2_dart_utils/dhis2_dart_utils.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dhis2_dart_utils.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dhis2_dart_utils;
+
+export 'src/dhis2_dart_utils_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
