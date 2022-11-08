48412af2c3f3b7aee33681c33707aa130f6172c6
megamind <echingalo@gmail.com>
2022-11-08 14:24:09 +0300

    refactor(exmaples): added the example for the program rules

--- example/dhis2_dart_utils_example.dart
+++ example/dhis2_dart_utils_example.dart
@@ -1,12 +1,18 @@
 import 'package:dhis2_dart_utils/dhis2_dart_utils.dart';
 import 'package:dhis2_dart_utils/src/utils/program_indicator_engine/models/program_indicator.dart';
+import 'package:dhis2_dart_utils/src/utils/program_rule_engine/models/program_rule.dart';
+import 'package:dhis2_dart_utils/src/utils/program_rule_engine/models/program_rule_variable.dart';
 
 void main() {
-  // UID generator example
+  // EXAMPLE: UID generator example
   print('Auto-generated UID from Core: ${Core.getUid()}');
 
-  // program indicator engine
-  Map dataObject = {"vV9UWAZohSf": 12, "qrur9Dvnyt5": 30, "GieVkTxp4HH": 0.4};
+  // EXAMPLE: program indicator engine
+  Map programIndicatorEngineDataObject = {
+    "vV9UWAZohSf": 12,
+    "qrur9Dvnyt5": 30,
+    "GieVkTxp4HH": 0.4,
+  };
   List<ProgramIndicator> programIndicators = [
     {
       "id": "yFTplrY7NaL",
@@ -17,8 +23,73 @@ void main() {
     }
   ].map((indicator) => ProgramIndicator.fromMap(indicator)).toList();
   print(
-      'Evaluation of $programIndicators based on $dataObject = ${ProgramIndicatorEngine.evaluateProgramIndicators(
-    programIndicators: programIndicators,
-    dataObject: dataObject,
-  )}');
+    'Evaluation of $programIndicators based on $programIndicatorEngineDataObject = ${ProgramIndicatorEngine.evaluateProgramIndicators(
+      programIndicators: programIndicators,
+      dataObject: programIndicatorEngineDataObject,
+    )}',
+  );
+
+  //  EXAMPLE: program rule engine
+  Map programRuleEngineDataObject = {
+    "oZg33kd9taw": "Male",
+  };
+  List<ProgramRule> programRules = [
+    {
+      "id": "fd3wL1quxGb",
+      "condition": "#{gender} == 'Male'",
+      "program": "eBAyeGv0exc",
+      "programRuleActions": [
+        {
+          "id": "IrmpncBsypT",
+          "programRuleActionType": "HIDEFIELD",
+          "programRule": "fd3wL1quxGb",
+          "dataElement": "SWfdB5lX0fk",
+          "programStageSection": "d7ZILSbPgYh"
+        }
+      ]
+    },
+    {
+      "id": "xOe5qCzRS0Y",
+      "condition": "!#{womanSmoking} ",
+      "program": "lxAQ7Zs9VYR",
+      "programRuleActions": [
+        {
+          "id": "hwgyO59SSxu",
+          "programRuleActionType": "HIDEFIELD",
+          "programRule": "xOe5qCzRS0Y",
+          "dataElement": "Ok9OQpitjQr"
+        }
+      ]
+    }
+  ]
+      .map((Map<String, dynamic> programRule) =>
+          ProgramRule.fromMap(programRule))
+      .toList();
+  List<ProgramRuleVariable> programRuleVariables = [
+    {
+      "name": "gender",
+      "id": "zINGRka3g9N",
+      "programRuleVariableSourceType": "DATAELEMENT_NEWEST_EVENT_PROGRAM",
+      "program": "eBAyeGv0exc",
+      "dataElement": "oZg33kd9taw"
+    },
+    {
+      "name": "womanSmoking",
+      "id": "Z92dJO9gIje",
+      "programRuleVariableSourceType": "DATAELEMENT_NEWEST_EVENT_PROGRAM",
+      "program": "lxAQ7Zs9VYR",
+      "dataElement": "sWoqcoByYmD"
+    }
+  ]
+      .map((Map<String, dynamic> programRuleVariable) =>
+          ProgramRuleVariable.fromMap(programRuleVariable))
+      .toList();
+
+  print(
+    'Evaluation of program rules $programRules based on variables $programRuleVariables on $programRuleEngineDataObject = ${ProgramRuleEngine.evaluateProgramRule(
+      programRules: programRules,
+      programRuleVariables: programRuleVariables,
+      dataObject: programRuleEngineDataObject,
+    )}',
+  );
 }
---
+++ test/utils/program_rule_engine/program_rule_engine_test.dart
@@ -0,0 +1,11 @@
+import 'package:dhis2_dart_utils/src/utils/program_rule_engine/models/program_rule.dart';
+import 'package:dhis2_dart_utils/src/utils/program_rule_engine/models/program_rule_variable.dart';
+import 'package:test/scaffolding.dart';
+
+List<ProgramRule> programRules = [];
+List<ProgramRuleVariable> programRuleVariables = [];
+Map dataObject = {};
+
+void main() {
+  group('Program rule engine test', () => {test('', () => {})});
+}
