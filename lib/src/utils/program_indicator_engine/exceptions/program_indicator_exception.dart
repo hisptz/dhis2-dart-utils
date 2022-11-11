49e23594622b4440e7430df8963943b61a3ede72
megamind <echingalo@gmail.com>
2022-11-11 11:06:25 +0300

    docs(program_indicator): added dartdocs for the module constants, models and helpers

--- lib/src/utils/program_indicator_engine/exceptions/program_indicator_exception.dart
+++ lib/src/utils/program_indicator_engine/exceptions/program_indicator_exception.dart
@@ -1,10 +1,23 @@
-class ProgramIndicatorException implements Exception {
-  final String cause;
+// Copyright (c) 2022, HISP Tanzania Developers.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
-  ProgramIndicatorException(this.cause);
+///
+/// This is the exception handler for the program indicator
+///
+class ProgramIndicatorException implements Exception {
+  ///
+  /// This is the source for the exception as description to what went wrong
+  ///
+  final String source;
+
+  ///
+  /// This is the constructor to the `ProgramIndicatorException`
+  ///
+  ProgramIndicatorException(this.source);
 
   @override
   String toString() {
-    return 'PROGRAM INDICATOR EXCEPTION: $cause';
+    return 'PROGRAM INDICATOR EXCEPTION: $source';
   }
 }
