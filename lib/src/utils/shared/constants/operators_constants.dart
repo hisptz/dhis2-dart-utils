c92165f1bab163bda3810f0f03416cf853c29534
megamind <echingalo@gmail.com>
2022-11-11 17:20:19 +0300

    docs(shared): added dartdoc to constants, helpers and exception files on the shared modules

--- lib/src/utils/shared/constants/operators_constants.dart
+++ lib/src/utils/shared/constants/operators_constants.dart
@@ -1,5 +1,15 @@
+// Copyright (c) 2022, HISP Tanzania Developers.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+///
+/// `OperatorsConstants` is a collection of supported operators for different computations by the library
+///
 class OperatorsConstants {
+  ///This is a list of arithmetic operators supported by the library
   static List<String> arithmeticOperators = ['+', '-', '*', '/', '%'];
+
+  // This is a list of all logical operators supported by the library
   static List<String> logicalOperators = [
     '>=',
     '===',
--- lib/src/utils/shared/helpers/data_object_helper.dart
+++ lib/src/utils/shared/helpers/data_object_helper.dart
@@ -1,6 +1,17 @@
+// Copyright (c) 2022, HISP Tanzania Developers.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import '../exceptions/data_object_exception.dart';
 
+///
+/// `DataObjectHelper` is a collection of helper functions for manipulating the form data object
+///
 class DataObjectHelper {
+  ///
+  /// `DataObjectHelper.getValueFromDataObject` is a function for getting a value from the the provided data object
+  ///  The function takes id of type `String` and a `Map` form data object, and returns a string that is a representation of the value from the data object
+  ///
   static String getValueFromDataObject(String uid, Map dataObject) {
     // TODO Add a default value for when the uid is not in the dataObject
     var value = '0';
