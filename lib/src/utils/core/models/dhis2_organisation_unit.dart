f68dfe0bd85493e325d6485195440ad534b18b73
megamind <echingalo@gmail.com>
2023-08-30 11:46:50 +0300

    refactor: added attribute for geometry in the organisation unit

--- lib/src/utils/core/models/dhis2_organisation_unit.dart
+++ lib/src/utils/core/models/dhis2_organisation_unit.dart
@@ -1,6 +1,8 @@
 // Copyright (c) 2022, HISP Tanzania Developers.
 // All rights reserved. Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:convert';
+
 ///
 /// `Dhis2OrganisationUnit` is a data model class for the DHIS2 Organisation unit metadata
 ///
@@ -33,6 +35,9 @@ class Dhis2OrganisationUnit {
   /// This is a list of DHIS2 ids for `Dhis2OrganisationUnit` that are children to the current organisation unit in the DHIS2 hierarchy
   List<String>? children;
 
+  /// This is a `Map` object that contains the geometry information for the organisation unit as configured in DHIS2 instance
+  Map<String, dynamic>? geometry;
+
   /// This is the default constructor for the `Dhis2OrganisationUnit`
   Dhis2OrganisationUnit({
     required this.id,
@@ -44,6 +49,7 @@ class Dhis2OrganisationUnit {
     this.dataSets = const [],
     this.programs = const [],
     this.children = const [],
+    this.geometry = const {},
   });
 
   /// `Dhis2OrganisationUnit.toMap` is a method that converts the `Dhis2OrganisationUnit` to a `Map` object
@@ -55,6 +61,7 @@ class Dhis2OrganisationUnit {
     data['code'] = code;
     data['level'] = level;
     data['path'] = path;
+    data['geometry'] = jsonEncode(geometry);
     return data;
   }
 
@@ -66,6 +73,7 @@ class Dhis2OrganisationUnit {
     code = mapData['code'];
     level = int.parse(mapData['level']);
     parent = mapData['parent'];
+    geometry = jsonDecode(mapData['geometry']);
   }
 
   /// `Dhis2OrganisationUnit.fromJson` is a factory constructor that generated a `Dhis2OrganisationUnit` from a `dynamic` json
@@ -76,6 +84,7 @@ class Dhis2OrganisationUnit {
     List<dynamic>? programList = json['programs'] as List<dynamic>?;
     List<dynamic>? dataSetList = json['dataSets'] as List<dynamic>?;
     List<dynamic>? childrenList = json['children'] as List<dynamic>?;
+    Map<String, dynamic> geometry = json['geometry'] ?? {};
     return Dhis2OrganisationUnit(
       id: json['id'],
       name: json['name'] ?? '',
@@ -87,6 +96,7 @@ class Dhis2OrganisationUnit {
       dataSets:
           (dataSetList ?? []).map((dataSet) => '${dataSet["id"]}').toList(),
       children: (childrenList ?? []).map((child) => '${child["id"]}').toList(),
+      geometry: geometry,
     );
   }
 
